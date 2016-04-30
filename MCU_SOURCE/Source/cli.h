7b5746bcf99661d7ce1a8f0aa804d33c9a08b46a
mocona05 <mocona05@gmail.com>
2016-04-30 10:11:19 +0900

    .

---
+++ MCU_SOURCE/Source/cli.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern bool cliMode;
+
+void cliProcess(void);
+
---
+++ MCU_SOURCE/Source/stm32f0xx_it.c
@@ -0,0 +1,156 @@
+/**
+  ******************************************************************************
+  * @file    IO_Toggle/stm32f0xx_it.c 
+  * @author  MCD Application Team
+  * @version V1.0.1
+  * @date    11-October-2013
+  * @brief   Main Interrupt Service Routines.
+  *          This file provides template for all exceptions handler and 
+  *          peripherals interrupt service routine.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "board.h"
+
+/** @addtogroup STM32F0308_Discovery_Peripheral_Examples
+  * @{
+  */
+
+/** @addtogroup IO_Toggle
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/******************************************************************************/
+/*            Cortex-M0 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+///**
+//  * @brief  This function handles SysTick Handler.
+//  * @param  None
+//  * @retval None
+//  */
+//void SysTick_Handler(void)
+//{
+//}
+
+void USART1_IRQHandler(void)
+{
+    uint8_t data;
+    
+    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+    {         
+
+//		Rxd_data(USART_ReceiveData(USART1));  
+//			UART1_PutChar(USART_ReceiveData(USART3));
+ 		if(enqueue(&Uart1RxQueue, USART_ReceiveData(USART1)) != QUEUE_SUCCESS)
+         {            
+             // 에러 처리
+         }           
+
+   }
+    if(USART_GetITStatus(USART1, USART_IT_TXE) != RESET)
+    {                
+        if(dequeue(&Uart1TxQueue, &data) == QUEUE_SUCCESS) 
+        {
+            /* Write one byte to the transmit data register */
+            USART_SendData(USART1, data);
+        }
+        else
+        {
+            /* Disable the USART1 Transmit interrupt */
+            USART_ITConfig(USART1, USART_IT_TXE, DISABLE);       
+        }              
+    }   
+}
+
+
+/******************************************************************************/
+/*                 STM32F0xx Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f0x8.s).                                               */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles PPP interrupt request.
+  * @param  None
+  * @retval None
+  */
+/*void PPP_IRQHandler(void)
+{
+}*/
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
